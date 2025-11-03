b932d965ed0d9c99294c96c1f7c92c51767f4072
tibotix <tizian@seehaus.net>
2025-11-03 19:14:42 +0100

    feat: Add `retrieve_from_doi` free method

--- pooch_doi/__init__.py
+++ pooch_doi/__init__.py
@@ -1,4 +1,4 @@
 # The version file is generated automatically by setuptools_scm
 from ._version import version as __version__
-from .core import DOIPooch
+from .core import DOIPooch, retrieve_from_doi
 from .repository import DataRepository
--- pooch_doi/core.py
+++ pooch_doi/core.py
@@ -1,6 +1,6 @@
-from typing import override
+from typing import Optional, override
 
-from pooch import Pooch
+from pooch import Pooch, retrieve
 from .repository import doi_to_repository
 from .utils import parse_doi
 
@@ -36,3 +36,28 @@ class DOIPooch(Pooch):
         # does not support availability checks for DOIDownloader
         # TODO: this could potentially be improved by using the availability feature of HTTPDownloader
         raise NotImplementedError("DOIPooch does not support availability checks.")
+
+
+def retrieve_from_doi(
+    doi: str,
+    known_hash: Optional[str] = None,
+    fname: Optional[str] = None,
+    path=None,
+    processor=None,
+    downloader=None,
+    progressbar: bool = False,
+) -> str:
+    # Resolve DOI
+    doi_netloc, doi_path = parse_doi(doi)
+    data_repository = doi_to_repository(doi_netloc)
+
+    # Remove the leading slash in the path and
+    # resolve the download URL
+    if doi_path[0] == "/":
+        doi_path = doi_path[1:]
+    download_url = data_repository.download_url(doi_path)
+
+    # Retrieve actual data file(s)
+    return retrieve(
+        download_url, known_hash, fname, path, processor, downloader, progressbar
+    )
