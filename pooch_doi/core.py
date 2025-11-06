7d133277b63f11ce6a55d4abefe9d9e330714545
tibotix <tizian@seehaus.net>
2025-11-06 19:56:08 +0100

    feature!: Populate known_hash in `retrieve_from_doi` from registry
    The current solution is just a workaround. We need to pass an object
    with a `registry` field to `DataRepository.populate_registry`. It would
    be nice to have a dedicated and specified `Registry` interface that would
    provide functions like
     - contains_file(file: str) -> bool
     - get_url_for_file(file: str) -> str
     - get_checksum_for_file(file: str) -> str
     - ...

--- pooch_doi/core.py
+++ pooch_doi/core.py
@@ -54,8 +54,8 @@ class DOIPooch(Pooch):
 
 def retrieve_from_doi(
     doi: str,
+    filename: str,
     known_hash: Optional[str] = None,
-    filename: Optional[str] = None,
     path=None,
     processor=None,
     downloader=None,
@@ -65,12 +65,18 @@ def retrieve_from_doi(
     assert_valid_doi(doi)
     data_repository = doi_to_repository(doi)
 
+    # TODO: THIS IS JUST A WORKAROUND!
+    #       Please refactor pooch to use a dedicated registry class and interface.
+    #       Then refactor this code here to use it.
+    dummy_pooch = Pooch("", "")
+    data_repository.populate_registry(dummy_pooch)
+    if filename not in dummy_pooch.registry:
+        raise ValueError(f"File '{filename}' not found in registry.")
+    known_hash = known_hash or dummy_pooch.registry[filename]
+
     # Resolve the download URL
     download_url = data_repository.download_url(doi)
 
-    # TODO: known_hash should never be None
-    # known_hash = data_repository.populate_registry(reg)
-    
     # Retrieve actual data file(s)
     return retrieve(
         download_url, known_hash, filename, path, processor, downloader, progressbar
