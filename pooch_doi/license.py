26ce74b4c65faaac8716d24478ca14b449e35828
tibotix <tizian@seehaus.net>
2026-01-15 01:40:00 +0100

    feat: Add License data model

--- pooch_doi/license.py
+++ pooch_doi/license.py
@@ -1,12 +1,35 @@
+from typing import List, Optional
+import dataclasses
+import enum
+
+
+class LicenseIdentifierScheme(enum.Enum):
+    SPDX = "spdx"
+    URL = "url"
+    OTHER = "other"
+
+
+@dataclasses.dataclass
+class LicenseIdentifier:
+    scheme: LicenseIdentifierScheme
+    value: str
+
+
+class LicenseReferenceRole(enum.Enum):
+    ICON = "icon"
+    TEXT = "text"
+    HOMEPAGE = "homepage"
+
+
+@dataclasses.dataclass
+class LicenseReference:
+    role: LicenseReferenceRole
+    uri: str
+
+
+@dataclasses.dataclass
 class License:
-    ...
-
-
-class SPDXLicense(License):
-    spdx_id: str
-
-
-class CustomLicense(License):
-    title: str
-    description: str
-    link: str
+    name: str
+    description: Optional[str] = None
+    identifiers: List[LicenseIdentifier] = dataclasses.field(default_factory=list)
+    references: List[LicenseReference] = dataclasses.field(default_factory=list)
