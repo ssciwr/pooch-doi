0896deb1c0ec56d2ff464f8a7559fb898d8b9216
tibotix <tizian@seehaus.net>
2026-01-15 02:04:58 +0100

    chore: Remove outdated TODO

--- pooch_doi/core.py
+++ pooch_doi/core.py
@@ -74,7 +74,6 @@ def retrieve_from_doi(
     download_url = data_repository.download_url(filename)
 
     # Retrieve actual data file(s)
-    # Todo: Patch this function for test purpose
     return retrieve(
         download_url, known_hash, filename, path, processor, downloader, progressbar
     )
