683267d6c9607e02dd3d0597d499cdcb18ee7f8f
tibotix <tizian@seehaus.net>
2026-01-22 00:14:18 +0100

    feat(testing): Add tests for `doi_to_repository` including allowed_exceptions.

--- tests/unit/test_repository.py
+++ tests/unit/test_repository.py
@@ -1,7 +1,9 @@
 import pytest
+import logging
 
 from pooch_doi.repository import (
     doi_to_url,
+    doi_to_repository,
     _get_all_available_data_repositories,
 )
 
@@ -50,5 +52,103 @@ def test_get_all_available_data_repositories(data_repo_manager, data_repo_factor
         assert _get_all_available_data_repositories() == []
 
 
-def test_doi_to_repository():
-    pass
+def test_doi_to_repository_with_valid_repo(
+    make_doi_resolve_to, data_repo_manager, data_repo_factory
+):
+    figshare_repo = (
+        data_repo_factory().with_initialize.match_domain("figshare.org").create_type()
+    )
+    zenodo_repo = (
+        data_repo_factory().with_initialize.match_domain("zenodo.org").create_type()
+    )
+    # make figshare first in chain-of-responsibility
+    data_repo_manager.make_available(figshare_repo, zenodo_repo)
+
+    doi = "10.5281/zenodo.4924875"
+    url = "https://zenodo.org/doi/10.5281/zenodo.4924875"
+    make_doi_resolve_to(doi, url)
+
+    assert isinstance(doi_to_repository(doi), zenodo_repo)
+
+
+def test_doi_to_repository_with_faulty_repo(
+    make_doi_resolve_to, data_repo_manager, data_repo_factory, caplog
+):
+    faulty_repo = data_repo_factory(typename="FaultyRepo")
+    faulty_repo = faulty_repo.with_issue_tracker("https://issuetracker.de")
+    faulty_repo = faulty_repo.with_allowed_exceptions(())  # no allowed exceptions
+    faulty_repo = faulty_repo.with_initialize.raise_exception(
+        ValueError("Test Exception")
+    )
+    faulty_repo = faulty_repo.create_type()
+    zenodo_repo = (
+        data_repo_factory().with_initialize.match_domain("zenodo.org").create_type()
+    )
+    # prioritize faulty_repo
+    data_repo_manager.make_available(faulty_repo, zenodo_repo)
+
+    doi = "10.5281/zenodo.4924875"
+    url = "https://zenodo.org/doi/10.5281/zenodo.4924875"
+    make_doi_resolve_to(doi, url)
+
+    caplog.set_level(logging.WARNING, logger="pooch-doi")
+    assert isinstance(doi_to_repository(doi), zenodo_repo)
+    assert caplog.record_tuples == [
+        (
+            "pooch-doi",
+            logging.WARNING,
+            "Repository Implementation 'FaultyRepo' failed with exception: 'Test Exception'. Please open an issue at 'https://issuetracker.de'.",
+        )
+    ]
+
+
+def test_doi_to_repository_with_allowed_faulty_repo(
+    make_doi_resolve_to, data_repo_manager, data_repo_factory
+):
+    faulty_repo = data_repo_factory()
+    faulty_repo = faulty_repo.with_name.return_value("faulty_repo")
+    faulty_repo = faulty_repo.with_issue_tracker("https://issuetracker.de")
+    faulty_repo = faulty_repo.with_allowed_exceptions(
+        (ValueError,)
+    )  # ValueError is allowed
+    faulty_repo = faulty_repo.with_initialize.raise_exception(
+        ValueError("Test Exception")
+    )
+    faulty_repo = faulty_repo.create_type()
+    zenodo_repo = (
+        data_repo_factory().with_initialize.match_domain("zenodo.org").create_type()
+    )
+    # prioritize faulty_repo
+    data_repo_manager.make_available(faulty_repo, zenodo_repo)
+
+    doi = "10.5281/zenodo.4924875"
+    url = "https://zenodo.org/doi/10.5281/zenodo.4924875"
+    make_doi_resolve_to(doi, url)
+
+    with pytest.raises(ValueError, match="Test Exception"):
+        doi_to_repository(doi)
+
+
+def test_doi_to_repository_with_user_warning(
+    make_doi_resolve_to, data_repo_manager, data_repo_factory
+):
+    figshare_repo = (
+        data_repo_factory().with_initialize.match_domain("figshare.org").create_type()
+    )
+    zenodo_repo = data_repo_factory()
+    zenodo_repo = zenodo_repo.with_name.return_value("zenodo_repo")
+    zenodo_repo = zenodo_repo.with_user_warning("Test Warning.")
+    zenodo_repo = zenodo_repo.with_initialize.match_domain("zenodo.org")
+    zenodo_repo = zenodo_repo.create_type()
+    # prioritize figshare_repo
+    data_repo_manager.make_available(figshare_repo, zenodo_repo)
+
+    doi = "10.5281/zenodo.4924875"
+    url = "https://zenodo.org/doi/10.5281/zenodo.4924875"
+    make_doi_resolve_to(doi, url)
+
+    with pytest.warns(
+        UserWarning,
+        match="Selected Repository 'zenodo_repo' issued a warning: Test Warning.",
+    ):
+        assert isinstance(doi_to_repository(doi), zenodo_repo)
